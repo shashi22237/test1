ba4508d0c75b5cb2f2cb74c3ed6bf2afba2cecab
shashi22237 <shashidharreddy.yalla@bdnt.tech>
2023-12-05 17:57:52 +0530

    intial

---
+++ assets/Contact.java
@@ -0,0 +1,14 @@
+
+package assets;
+
+/**
+ * JavaClass
+ */
+public class Contact {
+
+    public static void main(String[] args) {
+        System.out.println("preparing to send message...");
+
+    }
+
+}
